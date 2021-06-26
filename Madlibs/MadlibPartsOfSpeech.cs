b952efba7ea7b86fc3afdfac11907a3c93ad2879
Elizabeth Davis <elizabeth.davis@improving.com>
2021-06-26 15:27:58 -0500

    Version 1- Working

---
+++ Madlibs/MadlibPartsOfSpeech.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Madlibs
+{
+    class MadlibPartsOfSpeech
+    {
+        public string Noun { get; set; }
+        public string Verb { get; set; }
+        public string Verb2 { get; set; }
+        public string Adverb { get; set; }
+        public string Adverb2 { get; set; }
+        public string Adjective { get; set; }
+        public string NameofPet { get; set; }
+        public string TypeOfPet { get; set; }
+        public string Location { get; set; }
+        public string Story { get; set; }
+
+    }
+}
